644a61c73608e8a5fa1303cdd77845bb9a9b5435
Nectarios Chroniaris <nchroniaris@gmail.com>
2019-10-02 19:07:37 -0600

    Initial Project Commit

---
+++ app/src/main/java/edu/ualberta/cmput301f19t17/bigmood/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.ualberta.cmput301f19t17.bigmood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/test/java/edu/ualberta/cmput301f19t17/bigmood/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package edu.ualberta.cmput301f19t17.bigmood;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
