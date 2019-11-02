308d5f67fb41fc021047afc6322440840e76e40a
Nectarios Chroniaris <nchroniaris@gmail.com>
2019-11-02 00:40:28 -0600

    Update HomeViewModel

--- app/src/main/java/edu/ualberta/cmput301f19t17/bigmood/activity/HomeViewModel.java
+++ app/src/main/java/edu/ualberta/cmput301f19t17/bigmood/activity/HomeViewModel.java
@@ -2,8 +2,15 @@ package edu.ualberta.cmput301f19t17.bigmood.activity;
 
 import androidx.lifecycle.ViewModel;
 
+import edu.ualberta.cmput301f19t17.bigmood.database.User;
+
 /**
  * A class responsible for holding data that ALL the navigation fragments need to access. In particular this can include the currently logged in user.
  */
 public class HomeViewModel extends ViewModel {
+
+    // TODO: 2019-11-02 Nectarios: Implement 
+    private User currentUser;
+
+
 }
