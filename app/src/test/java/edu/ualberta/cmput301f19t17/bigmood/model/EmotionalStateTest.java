60a37fd3c442bb96a46729e5b154e594e69cdc0b
cMeltx3x <ryan_ch888@yahoo.ca>
2019-11-07 11:48:24 -0700

    - Completed EmotionalStateTest and SocialSituationTest - Removed mockito and robolectric from gradle (they are not needed)

---
+++ app/src/test/java/edu/ualberta/cmput301f19t17/bigmood/model/EmotionalStateTest.java
@@ -0,0 +1,40 @@
+package edu.ualberta.cmput301f19t17.bigmood.model;
+
+import org.junit.jupiter.api.Test;
+
+import edu.ualberta.cmput301f19t17.bigmood.R;
+import edu.ualberta.cmput301f19t17.bigmood.model.EmotionalState;
+
+import static org.junit.Assert.assertEquals;
+
+
+class EmotionalStateTest {
+
+    private EmotionalState mockEmotionalState() {
+        return EmotionalState.DISGUST;
+    }
+
+    @Test
+    void testGetStateCode() {
+        EmotionalState state = mockEmotionalState();
+        assertEquals(3, state.getStateCode());
+    }
+
+    @Test
+    void testGetDrawableId() {
+        EmotionalState state = mockEmotionalState();
+        assertEquals(R.drawable.ic_emoticon_disgust, state.getDrawableId());
+    }
+
+    @Test
+    void testToString() {
+        EmotionalState state = mockEmotionalState();
+        assertEquals("Disgusted", state.toString());
+    }
+
+    @Test
+    void testFindByStateCode() {
+        EmotionalState state = mockEmotionalState();
+        assertEquals(state, state.findByStateCode(3));
+    }
+}
