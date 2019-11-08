7f4119bb9e1dccb1ab6260fa46c0468cf2ac0524
Nectarios Chroniaris <nchroniaris@gmail.com>
2019-11-08 16:55:04 -0700

    Add Failing test to test Travis

---
+++ app/src/test/java/edu/ualberta/cmput301f19t17/bigmood/FailingTest.java
@@ -0,0 +1,16 @@
+package edu.ualberta.cmput301f19t17.bigmood;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class FailingTest {
+
+    @Test
+    public void failingTest() {
+
+        assertEquals(0, 1);
+
+    }
+
+}
