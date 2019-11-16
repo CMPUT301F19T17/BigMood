ce411e61cd8fdbcd0893cbd3f46f071f4d925c1c
TriNguyen <tlnguyen@ualberta.ca>
2019-11-15 18:19:38 -0700

    Change the idea of how to implement a map

--- app/src/main/java/edu/ualberta/cmput301f19t17/bigmood/fragment/dialog/MapDialogFragment.java
+++ app/src/main/java/edu/ualberta/cmput301f19t17/bigmood/fragment/dialog/MapDialogFragment.java
@@ -2,11 +2,17 @@ package edu.ualberta.cmput301f19t17.bigmood.fragment.dialog;
 
 import android.app.Dialog;
 import android.content.Context;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.appcompat.widget.Toolbar;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.DialogFragment;
 
@@ -17,6 +23,9 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
 
 import edu.ualberta.cmput301f19t17.bigmood.R;
+import edu.ualberta.cmput301f19t17.bigmood.activity.HomeActivity;
+import edu.ualberta.cmput301f19t17.bigmood.adapter.MoodAdapter;
+import edu.ualberta.cmput301f19t17.bigmood.model.Mood;
 
 /**
  * MapDialogFragment holds the MapView that is used to select the user's location when they choose to add an image to
@@ -25,47 +34,73 @@ import edu.ualberta.cmput301f19t17.bigmood.R;
 public class MapDialogFragment extends DialogFragment implements OnMapReadyCallback {
     private MapView mapView;
     private GoogleMap googleMap;
+    private MoodAdapter moodAdapter;
+    private Toolbar toolbar;
     private static final String MAP_VIEW_BUNDLE_KEY = "MapViewBundleKey";
 
 
+    /**
+     * of the on*()methods, this is the first. When we first want to create the dialog we set the theme to the fullscreen theme so that the edges match the parent. Here we also check for the existence of a mood in the arguments bundle and set it to our instance variable.
+     * @param savedInstanceState a bundle that holds the state of the fragment
+     */
     @Override
-    public Dialog onCreateDialog(Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         this.setStyle(DialogFragment.STYLE_NORMAL, R.style.AppTheme_FullScreenDialog);
+
+        // TODO: 11/15 Tri, the idea is to pass in the moodAdapter, get the location of every mood in it => put the marker on the map
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = inflater.inflate(R.layout.dialog_view_user_map, null, false);
+
 
         Bundle mapViewBundle = null;
         if (savedInstanceState != null) {
             mapViewBundle = savedInstanceState.getBundle(MAP_VIEW_BUNDLE_KEY);
         }
 
-        LayoutInflater inflater = (LayoutInflater) this.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-        View view = inflater.inflate(R.layout.dialog_view_user_map, null, false);
+        this.toolbar = view.findViewById(R.id.toolbar_view_map);
+        this.mapView = view.findViewById(R.id.map_view);
+        this.mapView.onCreate(mapViewBundle);
+        this.mapView.getMapAsync(this);
 
-        mapView = view.findViewById(R.id.map_view);
-        mapView.onCreate(mapViewBundle);
-        mapView.getMapAsync(this);
+        return view;
 
 
-        return this.buildDialog(view);
     }
 
+    @Override
+    public void onViewCreated(@NonNull final View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d(HomeActivity.LOG_TAG, "Close button clicked");
+                MapDialogFragment.this.dismiss();
+            }
+        });
+    }
 
+    @Override
+    public void onStart() {
+        super.onStart();
 
-    /**
-     * This method serves the purpose of building a proper dialog for this fragment. onCreateDialog() must return a dialog and this is where that occurs. The intention is that any subclasses will be able to override this method to implement their own dialogs.
-     * @param view This is the view that the Dialog has to attach to. We presume this has already been created.
-     * @return     Fully built Dialog
-     */
-    protected Dialog buildDialog(View view) {
+        Dialog dialog = this.getDialog();
 
-        // Define new AlertDialog.Builder so we can display a custom dialog.
-        AlertDialog.Builder builder = new AlertDialog.Builder(this.getContext());
+        if (dialog != null) {
 
-        // Build and return the dialog. We set the onClick listeners for each button to point to a different method in our interface which take different parameters
-        return builder
-                .setView(view)
-                .create();
+            int width = ViewGroup.LayoutParams.MATCH_PARENT;
+            int height = ViewGroup.LayoutParams.MATCH_PARENT;
 
+            dialog.getWindow().setLayout(width, height);
+            dialog.getWindow().setWindowAnimations(R.style.AppTheme_Slide);
+
+        }
     }
 
 
@@ -118,20 +153,5 @@ public class MapDialogFragment extends DialogFragment implements OnMapReadyCallb
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(ny));
     }
 
-    @Override
-    public void onStart() {
-        super.onStart();
 
-        Dialog dialog = this.getDialog();
-
-        if (dialog != null) {
-
-            int width = ViewGroup.LayoutParams.MATCH_PARENT;
-            int height = ViewGroup.LayoutParams.MATCH_PARENT;
-
-            dialog.getWindow().setLayout(width, height);
-            dialog.getWindow().setWindowAnimations(R.style.AppTheme_Slide);
-
-        }
-    }
 }
