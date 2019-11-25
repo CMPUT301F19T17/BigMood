cd3d1a5d351525badba9102ecbd7f68a8bb378fb
Tri Nguyen <tlnguyen@ualberta.ca>
2019-11-25 16:00:04 -0700

    Refactored US010501Test

--- app/src/androidTest/java/edu/ualberta/cmput301f19t17/bigmood/US010501Test.java
+++ app/src/androidTest/java/edu/ualberta/cmput301f19t17/bigmood/US010501Test.java
@@ -18,6 +18,7 @@ import org.junit.Test;
 
 import edu.ualberta.cmput301f19t17.bigmood.activity.AppPreferences;
 import edu.ualberta.cmput301f19t17.bigmood.activity.HomeActivity;
+import edu.ualberta.cmput301f19t17.bigmood.database.MockRepository;
 import edu.ualberta.cmput301f19t17.bigmood.database.MockUser;
 import edu.ualberta.cmput301f19t17.bigmood.model.EmotionalState;
 import edu.ualberta.cmput301f19t17.bigmood.model.SocialSituation;
@@ -28,67 +29,53 @@ import static org.junit.Assert.assertEquals;
 
 public class US010501Test {
     private Solo solo;
-    private AppPreferences appPreferences;
+    private static AppPreferences appPreferences;
 
-    @BeforeClass //runs before anything else runs
-    public static void setUpAppPrefs() throws Exception {
-        AppPreferences.getInstance().login(new MockUser("CMPUT301", "CMPUT", "301"));
+    private static MockRepository mockRepository;
+
+    @BeforeClass
+    public static void setRepository() {
+
+        // Set app preferences
+        US010501Test.appPreferences = AppPreferences.getInstance();
+
+        // Create new in-memory database and set the app preferences to use it
+        US010501Test.mockRepository = new MockRepository();
+        US010501Test.appPreferences.setRepository(US010501Test.mockRepository);
+
+        // Login with a user from the database using a specialized method in MockRepository
+        US010501Test.appPreferences.login(US010501Test.mockRepository.getUser("user1"));
+
     }
-
     @Rule
     public ActivityTestRule<HomeActivity> rule = new ActivityTestRule<>(HomeActivity.class, true, true);
 
-    @Before //runs before every test
+    @Before //Clears the mood list before each test
     public void setUp() throws Exception {
         solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
         appPreferences = AppPreferences.getInstance();
-
-//        appPreferences.getRepository().deleteAllMoods(appPreferences.getCurrentUser());
-        // TODO: 2019-11-06 Cameron:
-        solo.waitForText("HillyBillyBobTesterino", 0, 2000);
-    }
-    @AfterClass //runs after all tests have run
-    public static void cleanUp() {
-//        AppPreferences.getInstance().getRepository().deleteAllMoods(AppPreferences.getInstance().getCurrentUser());
+        solo.sleep(1000);
     }
 
     @Test
     public void checkDeleteMood() {
         solo.assertCurrentActivity("Wrong Activity", HomeActivity.class);
-
-        // TODO: 2019-11-06 Cameron:
-        //get message from async update before checking number of items in list
-        solo.waitForText("HillyBillyBobTesterino", 0, 2000);
+        solo.sleep(1000);
 
         ListView moodList = (ListView) solo.getView(R.id.mood_list);
         ListAdapter moodArrayAdapter = moodList.getAdapter();
 
         int originalNumListItems = moodArrayAdapter.getCount();
 
-        View fab = solo.getCurrentActivity().findViewById(R.id.floatingActionButton);
-
-        solo.clickOnView(fab);
-        solo.pressSpinnerItem(0, EmotionalState.DISGUST.getStateCode()); //disgusted
-        solo.pressSpinnerItem(1, SocialSituation.SEVERAL.getSituationCode()); //two to several
-
-        solo.enterText(((TextInputLayout) solo.getView(R.id.text_input_reason)).getEditText(), "check delete");
-
-        solo.clickOnView(solo.getView(R.id.action_save));
-        solo.waitForText(EmotionalState.DISGUST.toString(), 1, 1000);
-
-        solo.clickOnMenuItem("Disgust"); //select the mood we just created
-
+        //select mood3
+        solo.clickOnMenuItem("Happy");
         solo.clickOnButton("DELETE");
 
-        // TODO: 2019-11-06 Cameron:
-        solo.waitForText("HillyBillyBobTesterino", 0, 2000);
-
+        //wait for 1 seconds
+        solo.sleep(1000);
 
         //make sure there are no new elements in the list (ie, after we added the mood, it was deleted)
-        assertEquals(originalNumListItems, moodArrayAdapter.getCount());
-
-//        appPreferences.getRepository().deleteAllMoods(appPreferences.getCurrentUser());
-
+        assertEquals(originalNumListItems-1, moodArrayAdapter.getCount());
     }
 
 }
