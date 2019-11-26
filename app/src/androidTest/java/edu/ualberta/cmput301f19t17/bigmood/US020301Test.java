2eb3fe4424f4f75a8e38791ea52b664b36ae3491
TriNguyen <tlnguyen@ualberta.ca>
2019-11-25 22:30:15 -0700

    Refactored US020301Test

--- app/src/androidTest/java/edu/ualberta/cmput301f19t17/bigmood/US020301Test.java
+++ app/src/androidTest/java/edu/ualberta/cmput301f19t17/bigmood/US020301Test.java
@@ -15,6 +15,7 @@ import org.junit.Test;
 
 import edu.ualberta.cmput301f19t17.bigmood.activity.AppPreferences;
 import edu.ualberta.cmput301f19t17.bigmood.activity.HomeActivity;
+import edu.ualberta.cmput301f19t17.bigmood.database.MockRepository;
 import edu.ualberta.cmput301f19t17.bigmood.database.MockUser;
 import edu.ualberta.cmput301f19t17.bigmood.model.SocialSituation;
 
@@ -22,27 +23,33 @@ import static org.junit.Assert.assertTrue;
 
 public class US020301Test {
     private Solo solo;
-    private AppPreferences appPreferences;
+    private static AppPreferences appPreferences;
+    private static MockRepository mockRepository;
 
-    @BeforeClass //runs before anything else runs
-    public static void setUpAppPrefs() throws Exception {
-        AppPreferences.getInstance().login(new MockUser("CMPUT301", "CMPUT", "301"));
+    @BeforeClass
+    public static void setRepository() {
+
+        // Set app preferences
+        US020301Test.appPreferences = AppPreferences.getInstance();
+
+        // Create new in-memory database and set the app preferences to use it
+        US020301Test.mockRepository = new MockRepository();
+        US020301Test.appPreferences.setRepository(US020301Test.mockRepository);
+
+        // Login with a user from the database using a specialized method in MockRepository
+        US020301Test.appPreferences.login(US020301Test.mockRepository.getUser("user1"));
+        // Delete all previous mood
+        US020301Test.mockRepository.deleteAllUserMoods(US020301Test.mockRepository.getUser("user1"));
     }
 
     @Rule
     public ActivityTestRule<HomeActivity> rule = new ActivityTestRule<>(HomeActivity.class, true, true);
 
-    @Before //runs before every test
+    @Before //Clears the mood list before each test
     public void setUp() throws Exception {
         solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
         appPreferences = AppPreferences.getInstance();
-//        appPreferences.getRepository().deleteAllMoods(appPreferences.getCurrentUser());
-        // TODO: 2019-11-06 Cameron:
-        solo.waitForText("HillyBillyBobTesterino", 0, 1000);
-    }
-    @AfterClass //runs after all tests have run
-    public static void cleanUp() {
-//        AppPreferences.getInstance().getRepository().deleteAllMoods(AppPreferences.getInstance().getCurrentUser());
+        solo.sleep(1000);
     }
 
     @Test
@@ -51,7 +58,7 @@ public class US020301Test {
         View fab = solo.getCurrentActivity().findViewById(R.id.floatingActionButton);
         solo.clickOnView(fab);
 
-        solo.pressSpinnerItem(3, SocialSituation.SEVERAL.getSituationCode());
+        solo.pressSpinnerItem(3, SocialSituation.SEVERAL.getSituationCode()+1);
 
         solo.clickOnView(solo.getView(R.id.action_save));
         solo.waitForDialogToClose();
