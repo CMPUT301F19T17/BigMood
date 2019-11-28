80d8cfaedcf09ab4c8077330ea30ad42d1a948cc
sophiefeng <xfeng3@ualberta.ca>
2019-11-27 17:29:07 -0700

    updates on US050201

--- app/src/androidTest/java/edu/ualberta/cmput301f19t17/bigmood/US050201Test.java
+++ app/src/androidTest/java/edu/ualberta/cmput301f19t17/bigmood/US050201Test.java
@@ -16,6 +16,7 @@ import edu.ualberta.cmput301f19t17.bigmood.activity.AppPreferences;
 import edu.ualberta.cmput301f19t17.bigmood.activity.HomeActivity;
 import edu.ualberta.cmput301f19t17.bigmood.database.MockRepository;
 import edu.ualberta.cmput301f19t17.bigmood.database.MockUser;
+import edu.ualberta.cmput301f19t17.bigmood.database.User;
 
 import static org.junit.Assert.assertTrue;
 
@@ -48,24 +49,41 @@ public class US050201Test {
         solo = new Solo(InstrumentationRegistry.getInstrumentation(), rule.getActivity());
     }
 
+
+
     @Test
     public void acceptRequestTest(){
-        // Logged in as user2 who already has a pending request from user3
+
         solo.assertCurrentActivity("Wrong Activity", HomeActivity.class);
+        solo.clickOnText(solo.getCurrentActivity().getResources().getString(R.string.title_profile));
+
+        // user2 send a new request to user3
+        User requestedUser =  US050201Test.mockRepository.getUser("user3");
+        solo.typeText(((TextInputLayout) solo.getView(R.id.text_input_username)).getEditText(), requestedUser.getUsername());
+        solo.clickOnView(solo.getCurrentActivity().findViewById(R.id.button_request));
+        assertTrue(solo.waitForText(solo.getCurrentActivity().getResources().getString(R.string.toast_success_request_sent), 1, 2000));
+
+        // login as user3
+        US050201Test.appPreferences.getInstance().login(requestedUser);
+        solo.assertCurrentActivity("Wrong Activity", HomeActivity.class);
+        solo.clickOnText(solo.getCurrentActivity().getResources().getString(R.string.title_profile));
 
         // Go to Requests and accept the request from user3
         solo.clickOnText(solo.getCurrentActivity().getResources().getString(R.string.title_requests));
         solo.clickOnText(solo.getCurrentActivity().getResources().getString(R.string.label_request_accept));
-
         assertTrue(solo.waitForText(solo.getCurrentActivity().getResources().getString(R.string.toast_success_request_accept), 1, 2000));
 
-        // Go to Following and check that user3's recent mood is there
+        // login as user2, go to Following and check that user3's recent mood is there
+        User followedUser =  US050201Test.mockRepository.getUser("user2");
+        US050201Test.appPreferences.getInstance().login(followedUser);
         solo.clickOnText(solo.getCurrentActivity().getResources().getString(R.string.title_following));
-        // waiting on the implementation of displaying the username of the person being followed
+        solo.sleep(5000);
+
     }
 
     @Test
     public void rejectRequestTest(){
+
         // Logged in as user2 who already has a pending request from user3
         solo.assertCurrentActivity("Wrong Activity", HomeActivity.class);
 
@@ -77,8 +95,11 @@ public class US050201Test {
 
         // Go to Following and check that user3's recent mood is not there
         solo.clickOnText(solo.getCurrentActivity().getResources().getString(R.string.title_following));
-        // waiting on the implementation of displaying the username of the person being followed
+        solo.sleep(5000);
+
     }
+
+
 
     /**
      * Closes the activity after each test
