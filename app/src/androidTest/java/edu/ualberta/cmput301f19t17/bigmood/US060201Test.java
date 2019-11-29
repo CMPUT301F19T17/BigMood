0771c389d5c86ee8bd409e99aae1b8ddd3e505c6
TriNguyen <tlnguyen@ualberta.ca>
2019-11-29 04:25:19 -0700

    Minor deletions

--- app/src/androidTest/java/edu/ualberta/cmput301f19t17/bigmood/US060201Test.java
+++ app/src/androidTest/java/edu/ualberta/cmput301f19t17/bigmood/US060201Test.java
@@ -73,7 +73,7 @@ public class US060201Test {
 
 
     @Test
-    public void checkUserMap() throws UiObjectNotFoundException, InterruptedException {
+    public void checkUserMap() throws UiObjectNotFoundException {
 
         //go to the map view
         UiObject mapButton = device.findObject(new UiSelector()
@@ -81,8 +81,6 @@ public class US060201Test {
         if (mapButton.exists()) {
             mapButton.click(); //this will throw a UIObjectNotFoundException if we cannot click following
         }
-
-        Thread.sleep(100000);
 
         String user1MoodTime = "12:00";
 
