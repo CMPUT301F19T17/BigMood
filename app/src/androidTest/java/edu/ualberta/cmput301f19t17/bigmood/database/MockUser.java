08a9998492af05e39959b829c0d21cee5adc45f7
cameronridderikhoff <cameronridderikhoff@gmail.com>
2019-11-29 13:03:44 -0700

    Added Javadocs comments for all files other than the test files

--- app/src/androidTest/java/edu/ualberta/cmput301f19t17/bigmood/database/MockUser.java
+++ app/src/androidTest/java/edu/ualberta/cmput301f19t17/bigmood/database/MockUser.java
@@ -1,7 +1,16 @@
 package edu.ualberta.cmput301f19t17.bigmood.database;
 
+/**
+ * This is a class meant for testing. It is used by MockRepository to simulate having a user in Firestore.
+ */
 public class MockUser extends User {
 
+    /**
+     * Constructor for MockUser
+     * @param username the username of the MockUser
+     * @param firstname the first name of the MockUser
+     * @param lastname the last name of the MockUser
+     */
     public MockUser(String username, String firstname, String lastname) {
         super(username, firstname, lastname);
     }
